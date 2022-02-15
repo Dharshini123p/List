24f70d8d7b2747b44786654094c6f453481e342e
Dharshini123p <priyadharshiniaspd@gmail.com>
2022-02-15 16:30:44 +0530

    list 4:30 pm

---
+++ src/org/login/EmployeeInfo.java
@@ -0,0 +1,29 @@
+package org.login;
+
+public class EmployeeInfo {
+	public static void main(String[] args) {
+		String s = "Automation";
+		
+		int indexOf = s.indexOf('u');
+		System.out.println(indexOf);
+				
+		String upperCase = s.toUpperCase();
+		System.out.println(upperCase);
+				
+		String lowerCase = s.toLowerCase();
+		System.out.println(lowerCase);
+		
+		int length = s.length();
+		System.out.println(length);
+		
+		char charAt = s.charAt(5);
+		System.out.println(charAt);
+		
+		int lastIndexOf = s.lastIndexOf('o');
+		System.out.println(lastIndexOf);
+				
+	}
+	
+	
+
+}
